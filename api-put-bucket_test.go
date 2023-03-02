e90e8fc815eaa161cd74ca18cfcf902044b1d767
陆政 <lz@hztrinet.com>
2023-03-02 17:15:12 +0800

    <feat>: 支持创建bucket时设置ForceCreate

--- api-put-bucket_test.go
+++ api-put-bucket_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestClient_MakeBucket(t *testing.T) {
+func TestClient_MakeBucketPublicAccess(t *testing.T) {
 	c, err := New("127.0.0.1:19000", &Options{
 		Creds: credentials.NewStaticV4("minioadmin", "minioadmin", ""),
 	})
@@ -15,11 +15,34 @@ func TestClient_MakeBucket(t *testing.T) {
 	}
 	bucketName := "test-bucket-public-access-option"
 
-	err = c.makeBucket(context.Background(), bucketName, MakeBucketOptions{
+	err = c.MakeBucket(context.Background(), bucketName, MakeBucketOptions{
 		PublicAccess: true,
 	})
 	if err != nil {
 		t.Fatal(err)
 	}
-	//defer c.RemoveBucket(context.Background(), bucketName)
+	defer c.RemoveBucket(context.Background(), bucketName)
+}
+
+func TestClient_MakeBucketForceCreate(t *testing.T) {
+	c, err := New("127.0.0.1:19000", &Options{
+		Creds: credentials.NewStaticV4("minioadmin", "minioadmin", ""),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	bucketName := "test-bucket-force-create-option"
+
+	err = c.MakeBucket(context.Background(), bucketName, MakeBucketOptions{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = c.MakeBucket(context.Background(), bucketName, MakeBucketOptions{
+		ForceCreate: true,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer c.RemoveBucket(context.Background(), bucketName)
 }
